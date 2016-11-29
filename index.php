5f94d854ed0c11ec22042b476aa1876478133696
ichise <ichised@gmail.com>
2016-11-29 21:09:40 +0900

    おためし

---
+++ index.php
@@ -0,0 +1,12 @@
+//<!DOCTYPE html>
+//<html = lang = "ja">
+//	<head>
+//		<meta charset = "UTF-8">
+//		<title>おためし＋</title>
+//	</head>	
+//	<body>
+		<?php
+			echo "お試しですねー";
+		?>
+//	</body>
+//</html>
